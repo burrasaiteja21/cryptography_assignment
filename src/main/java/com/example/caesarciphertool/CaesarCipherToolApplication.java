f3cc7f4c4263cb0cf32d3905833daa39d547c12f
burrasaiteja21 <burra.saiteja21@gmail.com>
2024-04-04 23:43:39 +0530

    cryp

---
+++ src/main/java/com/example/caesarciphertool/CaesarCipherToolApplication.java
@@ -0,0 +1,13 @@
+package com.example.caesarciphertool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaesarCipherToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaesarCipherToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/caesarciphertool/CaesarCipherToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.caesarciphertool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaesarCipherToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
